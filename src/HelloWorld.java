bd451e6bdd92d012c4ed310b986fdba4a1b5e5e2
tbegg@emich.edu <tbegg@emich.edu>
2023-10-02 19:17:58 -0400

    Added more to second print

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-		System.out.println("second print statement/ alternate");
+		System.out.println("second print statement/ alternate now adding more");
 	}
 }
